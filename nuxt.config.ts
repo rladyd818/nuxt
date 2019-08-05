2fd1801d113a28e9fb1631fb745f5d2dabc9d1f5
rladyd818 <rladyd818@live.co.kr>
2019-08-05 09:32:14 +0900

    default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,25 @@
 export default {
-  env: {},
-  head: {
-    title: "practive",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js TypeScript project" }
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
-    ]
-  },
-  loading: { color: "#3B8070" },
-  css: ["~/assets/css/main.css"],
-  build: {},
-  modules: [
-    "@nuxtjs/axios",
-  ],
-  axios: {}
-}
+	env: {},
+	head: {
+		title: "practive",
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{
+				hid: "description",
+				name: "description",
+				content: "Nuxt.js TypeScript project"
+			}
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+	},
+	loading: { color: "#3B8070" },
+	css: ["~/assets/css/main.css"],
+	build: {},
+	modules: ["@nuxtjs/axios", "@nuxtjs/vuetify"],
+	vuetify: {
+		// Vuetify options
+		//  theme: { }
+	},
+	axios: {}
+};
